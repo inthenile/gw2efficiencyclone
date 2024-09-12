e403ffd242b2f70c47c83d490c45abba90b582cd
Salim Caliskan <joyceismyhomeboi@gmail.com>
2024-09-12 16:51:40 +0200

    got more work done in equipment tab. need some styling still

--- src/components/fetchedContent/accountInfo/characters/Equipment.tsx
+++ src/components/fetchedContent/accountInfo/characters/Equipment.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { act, useContext, useEffect, useState } from "react";
 import { KeyArrayContext } from "../../../../App";
 import styles from "./equipment.module.css"
 import placeholder from "./../../../../assets/placeholder.png"
@@ -6,18 +6,19 @@ import placeholder from "./../../../../assets/placeholder.png"
 type ItemType = {
     default_skin: number,
     description: string,
-    details?: {infix_upgrade?: { attributes: [{atttribute: string, modifier: number}]}},
+    details: {type?: string, weight_class?: string, infix_upgrade?: { attributes: [{atttribute: string, modifier: number}]}},
     icon: string,
     name: string,
     rarity: string,
     id: number,
     slot: string,
+    level?: number
 }
 
 type TabType = {
     name: string,
-    items: {id: number, slot: string}[],
-    tabNumber: number
+    equipment: {id: number, slot: string}[],
+    tab: number,
 }
 
 const Equipment = ({charName: charName} : {charName: string}) => {
@@ -26,21 +27,25 @@ const Equipment = ({charName: charName} : {charName: string}) => {
     const key = keyContext?.isMainKey;
 
     const [tabs, setTabs] = useState<TabType[]>([]);
-    const [tabNumber, setTabNumber] = useState<string>("1");
-    const [items, setItems] = useState<ItemType[]>([]);
+    const [tabNumber, setTabNumber] = useState<string>();
+    const [equippedItems, setequippedItems] = useState<ItemType[]>([]);
     const [itemIds, setItemIds] = useState<number[]>([])
     const [err, setErr] = useState(false);
     const [slots, setSlots] = useState<{slot: string, item: number}[]>([]);
 
     useEffect(() => {
         //to see how many tabs there are and save them in an array
-        fetch(`https://api.guildwars2.com/v2/characters/${charName}/equipmenttabs?access_token=${key?.key}&tabs=all`)
-
+        fetch(`https://api.guildwars2.com/v2/characters/${charName}?access_token=${key?.key}&v=latest`)
         .then(res =>{
             return res.json();            
         }).then(data =>{
-            data.map((x: any) => {
-                setTabs(t => [...t, {items: x.equipment, name: x.name, tabNumber: x.tab}])
+            const {active_equipment_tab} = data;
+            const {equipment_tabs} = data;
+            const {equipment} = data;
+            
+            setTabNumber(active_equipment_tab)
+            equipment_tabs.map((tab: TabType) => {
+                setTabs(t => [...t, {equipment: tab.equipment, name: tab.name, tab: tab.tab}]);
             })
         })
     }, [])
@@ -48,7 +53,7 @@ const Equipment = ({charName: charName} : {charName: string}) => {
     useEffect(() =>{
         fetchEquipmentTabs()
             return() => {
-                setItems([]);
+                setequippedItems([]);
                 setSlots([]);
                 setErr(false);
             }
@@ -70,7 +75,7 @@ const Equipment = ({charName: charName} : {charName: string}) => {
                 if (duplicateItems.length) {
                     checkDuplicates(data, duplicateItems);
                 } else {
-                    setItems(data);
+                    setequippedItems(data);
                 }
             })
         }
@@ -78,19 +83,19 @@ const Equipment = ({charName: charName} : {charName: string}) => {
     //DUPLLICATE IDS ARE CAUSING PROBLEMS: multiple objects with the same item ids are not fetched with the fetchItems function
     //with this function we create duplicates if the same id exists in our array
     function checkDuplicates(data: ItemType[], duplicateItems: number[]){
-        
             data.filter((item) => {
                 duplicateItems.forEach(element => {
                     if(item.id === element){
                         data.push(item);
                     }
                 });
-                
             })
-            setItems(data);
+            setequippedItems(data);
         }
+        
     function fetchEquipmentTabs(){
-        fetch(`https://api.guildwars2.com/v2/characters/${charName}/equipmenttabs/${tabNumber}?access_token=${key?.key}&v=latest`)
+        if (tabNumber) {
+            fetch(`https://api.guildwars2.com/v2/characters/${charName}/equipmenttabs/${tabNumber}?access_token=${key?.key}&v=latest`)
             .then(res =>{
                 return res.json();
             }).then(data =>{
@@ -103,6 +108,8 @@ const Equipment = ({charName: charName} : {charName: string}) => {
                     });
                 }             
             })
+        }
+
     }
 
     function handleTabChange(e: React.ChangeEvent<HTMLSelectElement>){
@@ -117,16 +124,16 @@ const Equipment = ({charName: charName} : {charName: string}) => {
                 <>
                     <h5>Equipment</h5>
                     <label> Equipment tab: </label>
-                    <select name="tabnum" id="tabnum" onChange={(e) => handleTabChange(e)}>
+                    <select  defaultValue={tabNumber} name="tabnum" id="tabnum" onChange={(e) => handleTabChange(e)}>
                         {tabs.map((tab, i) => (
-                        <option value={tab.tabNumber} key={i}> {tab.name ? tab.name : i+1} </option>
+                        <option value={tab.tab} key={i}> {tab.name ? tab.name : tab.tab} </option>
                         ))}
                     </select>
                 </>
-                {items && !err && <EquipmentLayout items={items} slots={slots}/>}
+                {equippedItems && !err && <EquipmentLayout items={equippedItems} slots={slots}/>}
             </>
             }
-            {!items?.length && !err && <p style={{fontStyle:"italic"}}>Fetching your equipment...</p>}
+            {!equippedItems.length && !err && <p style={{fontStyle:"italic"}}>Fetching your equipment...</p>}
             {err && <p>No items found</p>}
         </>
         
@@ -139,48 +146,46 @@ type EquipmentProp = {
     items: ItemType[],
     slots: {slot:string, item:number}[]
 }
+type EquippedSlotType = {
+    slot: string,
+    item: ItemType[]
+}
 export const EquipmentLayout = ({items, slots} : EquipmentProp) =>{
 
     const [layout, setLayout] = useState<any>();
     const [equippedSlots, setEquippedSlots] = useState<{slot: string, item: any}[]>([]);
 
-    const allSlots: string[] = [
-        "Helm", "Shoulders", "Gloves", "Coat", "Leggings", "Boots",
-        "WeaponA1", "WeaponA2", "WeaponB1", "WeaponB2",
-        "Backpack", "Accessory1", "Accessory2", "Amulet", "Ring1", "Ring2",
-        "HelmAquatic", "WeaponAquaticA", "WeaponAquaticB"
-    ]
 
     const initialIcons = {
-        Helm:           <img src={placeholder}></img>,
-        Shoulders:     <img src={placeholder}></img>,
-        Coat:          <img src={placeholder}></img>,
-        Gloves:        <img src={placeholder}></img>,
-        Leggings:      <img src={placeholder}></img>,
-        Boots:         <img src={placeholder}></img>,
-        WeaponA1 :      <img src={placeholder}></img>,
-        WeaponA2 :      <img src={placeholder}></img>,
-        WeaponB1 :      <img src={placeholder}></img>,
-        WeaponB2 :      <img src={placeholder}></img>,
-        Backpack :      <img src={placeholder}></img>,
-        Accessory1 :    <img src={placeholder}></img>,
-        Accessory2 :    <img src={placeholder}></img>,
-        Amulet:        <img src={placeholder}></img>,
-        Ring1:         <img src={placeholder}></img>,
-        Ring2:         <img src={placeholder}></img>,
-        HelmAquatic:   <img src={placeholder}></img>,
-        WeaponAquaticA: <img src={placeholder}></img>,
-        WeaponAquaticB: <img src={placeholder}></img>
+        Helm:           {image: <img src={placeholder}></img>, description:""},
+        Shoulders:      {image: <img src={placeholder}></img>, description:""},
+        Coat:           {image: <img src={placeholder}></img>, description:""},
+        Gloves:         {image: <img src={placeholder}></img>, description:""},
+        Leggings:       {image: <img src={placeholder}></img>, description:""},
+        Boots:          {image: <img src={placeholder}></img>, description:""},
+        WeaponA1 :      {image: <img src={placeholder}></img>, description:""},
+        WeaponA2 :      {image: <img src={placeholder}></img>, description:""},
+        WeaponB1 :      {image: <img src={placeholder}></img>, description:""},
+        WeaponB2 :      {image: <img src={placeholder}></img>, description:""},
+        Backpack :      {image: <img src={placeholder}></img>, description:""},
+        Accessory1 :    {image: <img src={placeholder}></img>, description:""},
+        Accessory2 :    {image: <img src={placeholder}></img>, description:""},
+        Amulet:         {image: <img src={placeholder}></img>, description:""},
+        Ring1:          {image: <img src={placeholder}></img>, description:""},
+        Ring2:          {image: <img src={placeholder}></img>, description:""},
+        HelmAquatic:    {image: <img src={placeholder}></img>, description:""},
+        WeaponAquaticA: {image: <img src={placeholder}></img>, description:""},
+        WeaponAquaticB: {image: <img src={placeholder}></img>, description:""}
      }
     const [icons, setIcons] = useState<{[key: string]: any}>(initialIcons)
 
-
+     
     useEffect(()=>{
 
         {items.length && slots.map((slot) => {
-           allSlots.includes(slot.slot) ? setEquippedSlots(s => [...s, {slot: slot.slot, item: items.filter(item => {
+           setEquippedSlots(s => [...s, {slot: slot.slot, item: items.filter(item => {
                 return item.id === slot.item;
-           })}]) : null;
+           })}]);
         })}
         return() => {
             setEquippedSlots([]);
@@ -198,21 +203,66 @@ export const EquipmentLayout = ({items, slots} : EquipmentProp) =>{
     }, [equippedSlots, setEquippedSlots])
     
 
-    function handleIcons(equippedSlots: {slot: string, item: any}[]){
+    function handleMouseEnter(item: EquippedSlotType, eqIcon: any){
+        eqIcon.description =  <div className={styles.activeCard}> {item.item[0].name} </div>
+        const element = document.getElementById(item.slot);
+        element?.classList.remove("inactiveCard");
+        console.log(eqIcon);
+        const {name, icon, rarity, level, details, description} = item.item[0];
+        const {type, weight_class} = details;
+        const card = {name, icon, rarity, level, description, type, weight_class};
+
+    }
+    function handleMouseExit(item: EquippedSlotType){
+
+        const element = document.getElementById(item.slot);
+        element?.classList.add("inactiveCard");
+
+    }
+
+    function handleIcons(equippedSlots: EquippedSlotType[]){
         let nextIcons: any = {...icons};
-        const keys = Object.keys(icons).map(slot => {
-            return slot;
-        });
 
         for (let i = 0; i < equippedSlots.length; i++) {
             const slot = equippedSlots[i].slot;
-            if (keys.includes(slot) ) {
                 equippedSlots.forEach(_slot => {
                     if (_slot.slot === slot) {
-                        nextIcons[slot] = <img src={_slot.item[0]?.icon} ></img>;
-                    } 
-                })
-            }
+                        const {item} = _slot;
+                    
+                        let borderColor;
+                        switch(_slot.item[0]?.rarity){
+                            case "Fine":
+                                borderColor = "blue";
+                                break;
+                            case "Masterwork":
+                                borderColor = "green";
+                                break;
+                            case "Rare":    
+                                borderColor = "yellow";
+                                break;
+                            case "Exotic":
+                                borderColor = "darkorange";
+                                break;
+                            case "Ascended":
+                                borderColor = "fuchsia";
+                                break;
+                            case "Legendary":
+                                borderColor = "purple";
+                                break;
+                            default:
+                                borderColor = "white";
+                    }
+                    console.log(slot);
+                    console.log(_slot);
+                    console.log(item[0]);
+                    
+                    
+                    nextIcons[slot] = {image: <img  onMouseEnter={() => handleMouseEnter(_slot, nextIcons[slot])}
+                                                    onMouseLeave={() => handleMouseExit(_slot)} 
+                                                    style={{border: `${borderColor} 2px solid`}} src={_slot.item[0]?.icon}></img>,
+                                        description: <div id={_slot.slot}className="inactiveCard"> {_slot.item[0].name} </div>};
+                } 
+            })
         }
         setIcons(nextIcons);
     }
@@ -220,33 +270,33 @@ export const EquipmentLayout = ({items, slots} : EquipmentProp) =>{
     useEffect(()=>{
 
         setLayout(
-                <div className={styles.equipment}>
+            <div className={styles.equipment}>
                 <div className={styles.armour}>
-                    {icons.Helm}
-                    {icons.Shoulders}
-                    {icons.Coat}
-                    {icons.Gloves}
-                    {icons.Leggings}
-                    {icons.Boots}
+                    <div>{icons.Helm.image}  </div>     {icons.Helm.description}
+                    <div>{icons.Shoulders.image}</div>  {icons.Shoulders.description} 
+                    <div>{icons.Coat.image}     </div>  {icons.Coat.description}   
+                    <div>{icons.Gloves.image}   </div>  {icons.Gloves.description} 
+                    <div>{icons.Leggings.image} </div>  {icons.Leggings.description} 
+                    <div>{icons.Boots.image}    </div>  {icons.Boots.description} 
                 </div>
                 <div className={styles.weapons}>
-                    {icons.WeaponA1}
-                    {icons.WeaponA2}
-                    {icons.WeaponB1}
-                    {icons.WeaponB2}
+                    <div>{icons.WeaponA1.image}</div> {icons.WeaponA1.description}
+                    <div>{icons.WeaponA2.image}</div> {icons.WeaponA2.description}
+                    <div>{icons.WeaponB1.image}</div> {icons.WeaponB1.description}    
+                    <div>{icons.WeaponB2.image}</div> {icons.WeaponB2.description}
                 </div>
                 <div className={styles.trinkets}>
-                    {icons.Backpack}
-                    {icons.Accessory1}
-                    {icons.Accessory2}
-                    {icons.Amulet}
-                    {icons.Ring1}
-                    {icons.Ring2}
+                    <div>{icons.Backpack.image}  </div> {icons.Backpack.description} 
+                    <div>{icons.Accessory1.image}</div> {icons.Accessory1.description}   
+                    <div>{icons.Accessory2.image}</div> {icons.Accessory2.description}
+                    <div>{icons.Amulet.image}    </div> {icons.Amulet.description} 
+                    <div>{icons.Ring1.image}     </div> {icons.Ring1.description} 
+                    <div>{icons.Ring2.image}     </div> {icons.Ring2.description} 
                 </div>
                 <div className={styles.aquatic}>
-                    {icons.HelmAquatic}
-                    {icons.WeaponAquaticA}
-                    {icons.WeaponAquaticB}
+                    <div>{icons.HelmAquatic.image}   </div>  {icons.HelmAquatic.description} 
+                    <div>{icons.WeaponAquaticA.image}</div>  {icons.WeaponAquaticA.description} 
+                    <div>{icons.WeaponAquaticB.image}</div>  {icons.WeaponAquaticB.description} 
                 </div>
             </div>
             )
@@ -255,8 +305,21 @@ export const EquipmentLayout = ({items, slots} : EquipmentProp) =>{
 
     return(
         <div> 
-            {equippedSlots && icons && layout && layout}
+            {equippedSlots.length > 0 && layout && layout}
         </div>
 
     )
 }
+    //function for hovering over items
+export function EquipmentCard({item}: {item: ItemType}){
+        const {name, icon, rarity, level, details, description} = item;
+        const {type, weight_class} = details;
+        
+        const card = {name, icon, rarity, level, description, type, weight_class};
+
+        console.log(card);
+        return(
+            <h1>Yes</h1>
+        )
+    }
+    
